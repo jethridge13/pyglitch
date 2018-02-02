2f2f8edf02dd72902e7125844b02045d4de147bd
Joshua Ethridge <joshua.ethridge13@gmail.com>
2018-02-01 23:45:33 -0500

    pixelLine and pixelLineWithWidth
    Creates lines of single color in a given distance. Can create of
    several widths

--- PixelSorter.py
+++ PixelSorter.py
@@ -1,5 +1,39 @@
 from PIL import Image
 
-class PixelSorter(self):
+def pixelLine(img, x, y, direction, length, color):
+	'''
+	Generates a pixel sorted line starting at x and y
+	and going for length in direction ('v' or 'h'
+	for vertical or horizontal) of a single color
+	img - the PIL Image object to edit
+	x - the x coordinate to start drawing on
+	y - the y coordinate to start drawing on
+	direction - 'v' or 'h', draw vertical or horizontal
+	length - number of pixels to draw for
+	'''
+	pixelMap = img.load()
 
-	
+	if direction == 'v':
+		for i in range(y, y+length):
+			pixelMap[x, i] = color
+	elif direction == 'h':
+		for i in range(x, x+length):
+			pixelMap[i, y] = color
+
+def pixelLineWithWidth(img, x, y, direction, length, color, width):
+	'''
+	Generates multiple pixel sorted lines starting at x and y
+	and going for length in direction ('v' or 'h'
+	for vertical or horizontal) of a single color
+	img - the PIL Image object to edit
+	x - the x coordinate to start drawing on
+	y - the y coordinate to start drawing on
+	direction - 'v' or 'h', draw vertical or horizontal
+	length - number of pixels to draw for
+	'''
+	if direction == 'v':
+		for i in range(x, x+width):
+			pixelLine(img, i, y, direction, length, color)
+	elif direction == 'h':
+		for i in range(y, y+width):
+			pixelLine(img, x, y, direction, length, color)
--- test.py
+++ test.py
@@ -1,4 +1,21 @@
 from PIL import Image
+import PixelSorter
 
-img = Image.new('RGBA', (250, 250), 'black')
-img.show()
+
+def test1():
+	img = Image.new('RGBA', (250, 250), 'black')
+	PixelSorter.pixelLine(img, 10, 10, 'v', 100, (255, 255, 255))
+	PixelSorter.pixelLine(img, 11, 11, 'v', 100, (255, 255, 255))
+	PixelSorter.pixelLine(img, 12, 12, 'v', 100, (255, 255, 255))
+	PixelSorter.pixelLine(img, 13, 13, 'v', 100, (255, 255, 255))
+	PixelSorter.pixelLine(img, 14, 14, 'v', 100, (255, 255, 255))
+	PixelSorter.pixelLine(img, 15, 15, 'v', 100, (255, 255, 255))
+	img.show()
+
+def test2():
+	img = Image.new('RGBA', (250, 250), 'black')
+	PixelSorter.pixelLineWithWidth(img, 10, 10, 'v', 100, (255, 255, 255), 30)
+	img.show()
+
+if __name__ == '__main__':
+	test2()
